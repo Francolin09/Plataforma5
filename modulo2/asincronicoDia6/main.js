76a739aeba11fe8d3f86d4096c6e03279ce9994d
Francolin09 <95132759+Francolin09@users.noreply.github.com>
2022-05-03 19:11:14 -0400

    Add files via upload

---
+++ modulo2/asincronicoDia6/main.js
@@ -0,0 +1,8 @@
+let peliculas = [{titulo:"Busqueda Implacable",rating:"5 estrellas",laHasVisto:"Viste"},
+                 {titulo:"Norbit",rating:"3 estrellas",laHasVisto:"No Viste"},
+                 {titulo:"Mini espías",rating:"2 estrellas",laHasVisto:"Viste"},
+                 {titulo:"La vida es bella",rating:"5 estrellas",laHasVisto:"No Viste"}];
+
+for(var i = 0; i<peliculas.length;i++){
+    console.log(peliculas[i].laHasVisto,peliculas[i].titulo,peliculas[i].rating)
+}                
---
+++ modulo2/asincronicoDia6/main2.js
@@ -0,0 +1,18 @@
+var arreglo =[];
+var objeto={};
+
+function ArraydeObjetos(palabra,numero){
+    
+    for(i=1;i<=numero;i++){
+        
+        
+         
+        arreglo.push({palabra:i})
+        
+    }
+    console.log(arreglo)
+
+}
+ArraydeObjetos("hola",8)
+
+
