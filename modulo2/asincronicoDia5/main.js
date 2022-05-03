e4c58d3c696bc3d250c4dcb7ec6e6780bcf51268
Francolin09 <95132759+Francolin09@users.noreply.github.com>
2022-05-03 16:26:43 -0400

    Add files via upload

---
+++ modulo2/asincronicoDia5/main.js
@@ -0,0 +1,15 @@
+var arreglo =[1,2,3,4];
+var sum =0;
+
+function sumArray(){
+    for(var i =0; i < arreglo.length; i++){
+        
+       sum+=arreglo[i]
+        
+    }
+    console.log(sum)
+    
+    
+} 
+
+sumArray();
